737d568ba2a2ef399054eea3b3aae3a99bf2e6f2
umashankar <umashankarnaidu07@gmail.com>
2026-05-12 10:39:18 +0530

    Update build.sh to use migrate:fresh --seed to resolve missing sessions table

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 composer install --no-dev --optimize-autoloader
-php artisan migrate --force
+php artisan migrate:fresh --seed --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
